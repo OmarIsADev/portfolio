112d42f7ee31d368571d97b031f59970cb19ff79
Omar Saad <64932754+gamerlord295@users.noreply.github.com>
2024-06-10 01:22:31 +0300

    Update Dropdown.js

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -83,16 +83,6 @@ function Dropdown({ isOpen, toggle }) {
           Contact
         </NavLink>
       </NavMenu>
-      <NavBtn onClick={toggle}>
-        <a
-          className="btn PrimaryBtn"
-          href="https://linkedin.com/in/pedro-sales-muniz"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Resume
-        </a>
-      </NavBtn>
     </SiderBar>
   );
 }
