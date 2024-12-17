097131f2579d258c2b65e042d797907241686d39
Omar Saad <saadomar343@gmail.com>
2024-12-17 14:44:00 +0200

    added vercel analytics

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
 import About from "../components/About/About";
 import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
-import Coffee from "../components/Coffee/Coffee";
 import FixSocialIcon from "../components/SocialIcon/FixSocialIcon";
 import ScrollToTop from "../components/SocialIcon/ScrollToTop";
 function Home() {
   return (
     <>
+      <Analytics />
       <Hero />
       <Projects />
       <About />
